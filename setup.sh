9c4293803b44e36974508e3992fc6a849e38ef7a
Alex Zhang <1309701864@qq.com>
2021-01-30 00:34:48 -0500

    Enter your message here

--- setup.sh
+++ setup.sh
@@ -12,9 +12,3 @@ enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
 
-
-git init
-git remote add origin https://github.com/lifuzhang1108/dementia_prediction_streamlit.git
-git add .
-git commit -m "inital commit"
-git push origin master

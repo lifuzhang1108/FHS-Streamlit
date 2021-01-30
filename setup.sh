bc0f13fd115b1a24fd30081c30e4789c5a71c00a
Alex Zhang <1309701864@qq.com>
2021-01-30 00:18:55 -0500

    inital commit

--- setup.sh
+++ setup.sh
@@ -11,3 +11,10 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+
+git init
+git remote add origin https://github.com/lifuzhang1108/dementia_prediction_streamlit.git
+git add .
+git commit -m "inital commit"
+git push origin master
